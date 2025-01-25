1894f0a192ee1d569486b740aa2e4d4b4c7f31e1
NunoRJ18 <nunomartinsdocouto@gmail.com>
2025-01-24 22:20:21 -0300

    Update grafoTAD.h

--- grafoTAD.h
+++ grafoTAD.h
@@ -1,10 +1,16 @@
+#ifndef GRAFOTAD_H
+#define GRAFOTAD_H
+
 typedef struct grafo* Grafo;
 
 
 Grafo Inicializar(void);
 Grafo Destruir (Grafo G);
-void Inserir_Aresta(Grafo G,int chave,void* objeto,int SizeObj,int origem);
+void Inserir_Aresta(Grafo G,int destino,void* objeto,int SizeObj,int origem);
 void Remover_Aresta(Grafo G,void* objeto,int sizeObj,int origem,int destino);
-void Grau_Vertice(Grafo G,int vertice);
+int Grau_Vertice(Grafo G,int vertice);
 void Imprimir_Grafo(Grafo G);
 void* Search(Grafo G,int chave,int origem);
+
+
+#endif //GRAFOTAD_H
